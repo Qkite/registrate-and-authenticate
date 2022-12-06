56f7b259dbb03f49498e5952bafa45bf011d7670
Qkite <yeonjigu4@gmail.com>
2022-12-06 11:25:24 +0900

    Initial commit

---
+++ src/main/java/com/springsecurity/registrateandauthenticate/RegistrateAndAuthenticateApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.registrateandauthenticate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrateAndAuthenticateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrateAndAuthenticateApplication.class, args);
+    }
+
+}
